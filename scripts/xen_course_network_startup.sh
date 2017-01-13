cadf8e75303bc571494d7a6827cd8245aa8f177a
Richie Johnson <richiedjohnson@yahoo.co.in>
2017-01-13 02:46:18 -0500

    fixed code to create bonds and bridges only for class nets

--- scripts/xen_course_network_startup.sh
+++ scripts/xen_course_network_startup.sh
@@ -20,7 +20,7 @@ function add_bridge_if {
 host=$(awk -F ":" '/VITAL_DB_HOST/ {print $2}' /home/vlab/config.ini | tr -d ' ')
 pass=$(awk -F ":" '/VITAL_DB_PWD/ {print $2}' /home/vlab/config.ini | tr -d ' ')
 
-nets=$(PGPASSWORD=$pass psql -U postgres -d vital_db -h $host -t -c "SELECT c.id from vital_course c join vital_network_configuration n on c.id=n.course_id where c.status='ACTIVE'")
+nets=$(PGPASSWORD=$pass psql -U postgres -d vital_db -h $host -t -c "SELECT c.id from vital_course c join vital_network_configuration n on c.id=n.course_id where c.status='ACTIVE' and n.is_course_net=True")
 set -f
 array=(${nets// / })
 
