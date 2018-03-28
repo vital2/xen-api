7898ea6fcf2bffccd13814b8ae33a72bceb24c5b
Anoop <as11552@nyu.edu>
2018-03-27 22:47:25 -0400

    Added security to the internal API Call. * NOTE: REQUIRES CHANGES TO THE CONFIG FILE ON BOTH WEBSERVER AND XEN MACHINES.

--- monitor_XenVM.py
+++ monitor_XenVM.py
@@ -1,6 +1,7 @@
 import sys
 import logging
 import requests
+import ConfigParser
 
 from logging.handlers import RotatingFileHandler
 from pyxs import Client, PyXSError
@@ -12,6 +13,12 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+config_ini = ConfigParser.ConfigParser()
+config_ini.optionxform=str
+
+# TODO change to common config file in shared location
+config_ini.read("/home/vlab/config.ini")
+
 with Client() as c:
     # the sys.arg is the domid which is to be passed to the function call
     dom_id = int(sys.argv[1])
@@ -21,17 +28,16 @@ with Client() as c:
     logger.debug('VM {}, {}'.format(user_id, vm_id))
     path = c.get_domain_path(dom_id)
     path = path + '/control/shutdown'
+    api_key = config_ini.get('Security', 'INTERNAL_API_KEY')
+    logger.debug('{}: {}'.format(config_ini.get("VITAL", "SERVER_NAME"), api_key))
 
     with c.monitor() as m:
 	# watch for any random string
-        m.watch(path, b"baz")
+        m.watch(path, b'baz')
         logger.debug('Watching path {}'.format(path))
         next(m.wait())
         # print(next(m.wait()))
         if next(m.wait()) is not None:
             logger.debug('Event on path {}'.format(path))
-            params = {'user_id': user_id, 'vm_id': vm_id}
-	    requests.get("https://vital-dev2.poly.edu/vital/users/release-vm/", params=params)
-        # Do the necassary action like calling the script
-        # the script may call the vital api to do th enecessary actions
-        # maybe we can also send the required domid along with the request 
+            params = {'api_key': api_key, 'user_id': user_id, 'vm_id': vm_id}
+	    requests.get('https://' + config_ini.get("VITAL", "SERVER_NAME") + '/vital/users/release-vm/', params=params)
