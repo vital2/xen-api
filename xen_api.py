a9037f450bc36d4af4ede63501a99122752fad58
Anoop <as11552@nyu.edu>
2018-06-30 01:47:05 -0400

    Removed Monitor Xen Script and updated the functionality in Xen API as a background thread. * Updated on a suggestion in vm-shutdown branch. * Reduces reliance on the linux backend job scheduling.

--- xen_api.py
+++ xen_api.py
@@ -6,9 +6,11 @@ import socket
 import sys
 import ConfigParser
 import logging
+import requests
 
 from logging.handlers import RotatingFileHandler
 from pyxs import Client, PyXSError
+from threading import Thread
 
 config = ConfigParser.ConfigParser()
 
@@ -39,19 +41,23 @@ class XenAPI:
         """
         if not self.vm_exists(vm_name):
             logger.debug('Starting VM - {}'.format(vm_name))
-            return VirtualMachine(vm_name).start(vm_options)
+            vm = VirtualMachine(vm_name).start(vm_options)
         else:
             logger.debug('VM already Exists - {}'.format(vm_name))
             vm = self.list_vm(vm_name, None)
 
             # Start the Monitor Xen VM Script to watch the Xenstored Path
             # And let it run in the background we are not worried about collecting the results
-            cmd = '{} {}/monitor_XenVM.py {}'.format(
-                sys.executable, os.path.dirname(os.path.realpath(__file__)), vm.id)
-            logger.debug('Watching VM with Xenstore {}'.format(cmd))
-            Popen(cmd.split(), close_fds=True)
+            # cmd = '{} {}/monitor_XenVM.py {}'.format(
+            #     sys.executable, os.path.dirname(os.path.realpath(__file__)), vm.id)
+            # logger.debug('Watching VM with Xenstore {}'.format(cmd))
+            # Popen(cmd.split(), close_fds=True)
+            # Using Threading Module to send the function to background
 
-            return vm
+        background_thread = Thread(target=self.listenToVMShutdown, args=(vm.id,))
+        background_thread.start()
+
+        return vm
 
     def stop_vm(self, vm_name):
         """
@@ -255,6 +261,31 @@ class XenAPI:
         else:
             return False
 
+    def listenToVMShutdown(self, dom_id):
+        with Client() as c:
+          # the sys.arg is the domid which is to be passed to the function call
+          # dom_id = int(sys.argv[1])
+          dom_name = c['/local/domain/{}/name'.format(dom_id)]
+          user_id = dom_name.split('_')[0]
+          vm_id = dom_name.split('_')[2]
+          logger.debug('VM {}, {}'.format(user_id, vm_id))
+          path = c.get_domain_path(dom_id)
+          path = path + '/control/shutdown'
+          api_key = config.get('Security', 'INTERNAL_API_KEY')
+          logger.debug('{}: {}'.format(config.get("VITAL", "SERVER_NAME"), api_key))
+
+          with c.monitor() as m:
+            # watch for any random string
+            m.watch(path, b'baz')
+            logger.debug('Watching path {}'.format(path))
+            next(m.wait())
+
+            if next(m.wait()) is not None:
+                logger.debug('Event on path {}'.format(path))
+                params = {'api_key': api_key, 'user_id': user_id, 'vm_id': vm_id}
+
+            requests.get('https://' + config.get("VITAL", "SERVER_NAME") + '/vital/users/release-vm/', params=params)
+
 
 class VirtualMachine:
     """
@@ -301,10 +332,10 @@ class VirtualMachine:
 
             # Start the Monitor Xen VM Script to watch the Xenstored Path
             # And let it run in the background we are not worried about collecting the results
-            cmd = '{} {}/monitor_XenVM.py {}'.format(
-                sys.executable, os.path.dirname(os.path.realpath(__file__)), vm.id)
-            logger.debug('Watching VM with Xenstore {}'.format(cmd))
-            Popen(cmd.split(), close_fds=True)
+            # cmd = '{} {}/monitor_XenVM.py {}'.format(
+            #    sys.executable, os.path.dirname(os.path.realpath(__file__)), vm.id)
+            # logger.debug('Watching VM with Xenstore {}'.format(cmd))
+            # Popen(cmd.split(), close_fds=True)
 
             return vm
 
